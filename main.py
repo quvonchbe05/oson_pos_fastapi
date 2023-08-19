4410cd8a53d6ea8fe9207e54ed57e6b90afc591b
Quvonchbek Murodov <quvonchbe05@gmail.com>
2023-08-19 23:38:07 +0500

    added cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers.auth import router as auth_router
 from api.routers.users import router as user_router
 from api.routers.categories import router as categry_router
@@ -13,3 +14,13 @@ app.include_router(categry_router)
 app.include_router(product_router)
 app.include_router(basket_router)
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
